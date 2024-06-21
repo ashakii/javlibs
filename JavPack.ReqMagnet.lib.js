22f50947a13849537bf4c50982ff65f5250214fe
zyashakii <39474823+ashakii@users.noreply.github.com>
2024-06-21 18:31:54 +0800

    Update JavPack.ReqMagnet.lib.js

--- JavPack.ReqMagnet.lib.js
+++ JavPack.ReqMagnet.lib.js
@@ -19,46 +19,13 @@ class ReqMagnet extends Req {
       [
         (dom) => {
           return [...dom.querySelectorAll(".one_result")]
-            。map((item) => {
+            .map((item) => {
               return {
                 name: item.querySelector(".torrent_name").textContent,
                 url: item.querySelector(".torrent_magnet a").href,
                 size: item.querySelector(".torrent_size").textContent.replace(spaceReg, ""),
                 files: item.querySelector(".torrent_files")?.textContent ?? "1",
                 date: item.querySelector(".torrent_age").textContent,
-              };
-            })
-            。filter(({ name }) => name.toUpperCase().includes(code));
-        },
-      ],
-    );
-  };
-
-  static btsow(code) {
-    const spaceReg = /\s/g;
-    const host = "https://btsow.motorcycles";
-
-    return this.tasks(
-      {
-        url: `${host}/search/${encodeURIComponent(code)}`,
-        headers: {
-          "Referer": `${host}/index.htm`,
-          "Sec-Fetch-Dest": "document",
-          "Sec-Fetch-Mode": "navigate",
-          "Sec-Fetch-Site": "same-origin",
-          "Sec-Fetch-User": "?1",
-          "Upgrade-Insecure-Requests": "1",
-        },
-      },
-      [
-        (dom) => {
-          return [...dom.querySelectorAll(".data-list")]
-            .map((item) => {
-              return {
-                name: item.querySelector(".row a").title,
-                url: item.querySelector(".row a").href,
-                size: item.querySelector(".col-sm-2.text-right.size").textContent.replace(spaceReg, "")，              
-                date: item.querySelector(".col-sm-2.text-right.date").textContent,
               };
             })
             .filter(({ name }) => name.toUpperCase().includes(code));
