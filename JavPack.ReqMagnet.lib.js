f60403aae9a38a03fe665407ebf4b712cc946951
zyashakii <39474823+ashakii@users.noreply.github.com>
2024-06-24 13:56:18 +0800

    Update JavPack.ReqMagnet.lib.js

--- JavPack.ReqMagnet.lib.js
+++ JavPack.ReqMagnet.lib.js
@@ -33,4 +33,38 @@ class ReqMagnet extends Req {
       ],
     );
   }
+
+  static btsow(code) {
+    const spaceReg = /\s/g;
+    const host = "https://btsow.motorcycles/";
+
+    return this.tasks(
+      {
+        url: `${host}/${code}`,
+        headers: {
+          "Referer": `${host}/index.htm`,
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-User": "?1",
+          "Upgrade-Insecure-Requests": "1",
+        },
+      },
+      [
+        (dom) => {
+          return [...dom.querySelectorAll(".data-list")]
+            .map((row) => {
+              return {
+                name: row.querySelector(".row a").title,
+                url: `magnet:?xt=urn:btih:${row.querySelector('.row a').href.match(/hash\/([a-fA-F0-9]+)$/)[1] ?? ''}`,
+                size: row.querySelector(".row .size").textContent.replace(spaceReg, ""),
+                files: "1.8",
+                date: row.querySelector(".row .date").textContent,
+              };
+            })
+            .filter(({ name }) => name.toUpperCase().includes(code));
+        },
+      ],
+    );
+  }
 }
