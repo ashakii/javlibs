63890a4aeaeb7549580ad07f4d48008463be6001
zyashakii <39474823+ashakii@users.noreply.github.com>
2024-11-02 22:19:39 +0800

    Update JavPack.Util.lib.js

--- libs/JavPack.Util.lib.js
+++ libs/JavPack.Util.lib.js
@@ -16,11 +16,22 @@ class Util {
   }
 
   static codeParse(code) {
-    // 修改分割逻辑，同时处理横线、下划线和空格
-    const codes = code.split(/[-_\s]+/);
+    // 首先检查是否是标准番号格式（包含数字的情况）
+    const hasNumbers = /\d/.test(code);
 
-    // 修改分隔符模式，增加对空格的支持
-    const sep = "\\s*(0|-|_|\\s){0,2}\\s*";
+    let codes;
+    if (hasNumbers) {
+      // 对于标准番号格式，只用横线和下划线分割
+      codes = code.split(/-|_/);
+    } else {
+      // 对于非标准格式（如Titty Attack），使用空格、横线和下划线分割
+      codes = code.split(/[-_\s]+/);
+    }
+
+    // 根据不同情况使用不同的分隔符模式
+    const sep = hasNumbers
+      ? "\\s?(0|-|_){0,2}\\s?"  // 标准番号格式的分隔符
+      : "\\s*(0|-|_|\\s){0,2}\\s*";  // 非标准格式的分隔符
 
     let pattern = codes.join(sep);
 
@@ -28,11 +39,15 @@ class Util {
     if (/^fc2/i.test(code)) pattern = `${codes[0]}${sep}(ppv)?${sep}${codes.at(-1)}`;
     if (/^heyzo/i.test(code)) pattern = `${codes[0]}${sep}(\\w){0,2}${sep}${codes.at(-1)}`;
 
+    // 根据不同情况使用不同的正则边界
+    const regex = hasNumbers
+      ? new RegExp(`(?<![a-z])${pattern}(?!\\d)`, "i")  // 标准番号格式的正则
+      : new RegExp(`(?<![a-z])(${pattern})(?![\\w-])`, "i");  // 非标准格式的正则
+
     return {
       codes,
       prefix: codes[0],
-      // 修改正则表达式，使其更灵活地处理边界情况
-      regex: new RegExp(`(?<![a-z])(${pattern})(?![\\w-])`, "i"),
+      regex
     };
   }
 
